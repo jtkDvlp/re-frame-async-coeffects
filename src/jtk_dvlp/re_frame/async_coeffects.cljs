d320ba96b7cdfbdaabe64ac4e02c1e32a7a5852a
jtkDvlp <jtkdevelopments@gmail.com>
2021-12-25 15:48:32 +0100

    Adds fn to register async coeffect

--- src/jtk_dvlp/re_frame/async_coeffects.cljs
+++ src/jtk_dvlp/re_frame/async_coeffects.cljs
@@ -1,1 +1,20 @@
-(ns jtk-dvlp.re-frame.async-coeffects)
+(ns jtk-dvlp.re-frame.async-coeffects
+  (:require
+   [cljs.core.async]
+   [re-frame.registrar :refer [register-handler get-handler]]))
+
+
+(def kind :acofx)
+
+(defn reg-acofx
+  "Register the given async-coeffect `handler` for the given `id`, for later use
+  within `inject-acofx`:
+
+    - `id` is keyword, often namespaced.
+    - `handler` is a function which takes either one or two arguements, the first of which is
+       always `coeffects` and which returns an updated `coeffects` as `cljs.core.async/promise-chan`.
+
+  See also: `inject-acofx`
+  "
+  [id handler]
+  (register-handler kind id handler))
